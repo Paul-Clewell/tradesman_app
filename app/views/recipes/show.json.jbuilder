dbd2bdf60ae7dd0c3fa6b99cf3fe0b2ca0d7da70
Paul Clewell <andrevarshea@gmail.com>
2014-03-02 21:38:00 -0500

    First commit.

---
+++ app/views/recipes/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @recipe, :id, :title, :level, :resources, :components, :crafted_materials, :guilds, :created_at, :updated_at
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TradesmanApp::Application.config.session_store :cookie_store, key: '_tradesman_app_session'
