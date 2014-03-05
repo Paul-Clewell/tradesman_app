45b30e81b34f7693e40915cb5e0e99ec54660a0a
Paul Clewell <paulclewell@Macintosh-4.local>
2014-03-05 10:22:48 -0500

    Committing layout and stylesheet changes, as well as setting the base route to recipes/index.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 TradesmanApp::Application.routes.draw do
   resources :recipes
 
-  #root ''
+  root 'recipes#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
